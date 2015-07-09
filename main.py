14cefd65eeadc10659f69778f9491df0f4c38629
Andy Fraley <andy_fraley@hotmail.com>
2015-07-08 22:12:28 -0400

    Add changed files

--- main.py
+++ main.py
@@ -3,16 +3,16 @@
 import sys, getopt
 
 def usage():
-        print 'Usage: convert.py --individual-file <individual_file> --family-file <family_file>\n'\
-              '    --contribution-file <contribution_file> --output-file <output_file>\n\n'\
+        print 'Usage: convert.py --individual-filename <individual_filename> --family-filename <family_filename>\n'\
+              '    --contribution-filename <contribution_filename> --output-filename <output_filename>\n\n'\
               '    where\n\n'\
-              '        <individual_file> is a CSV file extract from Servant Keeper containing one line per '\
+              '        <individual_filename> is a CSV file extract from Servant Keeper containing one line per '\
               'individual\n\n'\
-              '        <family_file> is a CSV file extract from Servant Keeper containing one line per '\
+              '        <family_filename> is a CSV file extract from Servant Keeper containing one line per '\
               'family\n\n'\
-              '        <contribution_file> is a CSV file extract from Servant Keeper containing one line per '\
+              '        <contribution_filename> is a CSV file extract from Servant Keeper containing one line per '\
               'contribution\n\n'\
-              '        <output_file> is the transformed output Excel loading file for CCB\n\n'\
+              '        <output_filename> is the transformed output Excel loading file for CCB\n\n'\
               '    NOTE: There must be at least one each individual_file, family_file, and contribution_file as \n'\
               '    input files.  However, contents for individuals, familys, and contributions can be split across\n'\
               '    multiple files each like:\n\n'\
@@ -22,13 +22,14 @@ def usage():
               '    object type and (2) there are no duplicate columns for any given object ID.'
 
 def main(argv):
-    individual_file = ''
-    family_file = ''
-    contribution_file = ''
-    output_file = ''
+    individual_filenames = []
+    family_filenames = []
+    contribution_filenames = []
+    output_filename = ''
 
     try:
-        opts, args = getopt.getopt(argv,"",["individual-file=","family-file=","contribution-file=","output-file=","help"])
+        opts, args = getopt.getopt(argv,"",["individual-filename=","family-filename=","contribution-filename=",\
+                                            "output-filename=","help"])
 
     except getopt.GetoptError:
         usage()
@@ -39,26 +40,30 @@ def main(argv):
             usage()
             sys.exit()
 
-        elif opt in ("--individual-file"):
+        elif opt in ("--individual-filename"):
             individual_file = arg
+            individual_filenames.append(arg)
 
-        elif opt in ("--family-file"):
+        elif opt in ("--family-filename"):
             family_file = arg
+            family_filenames.append(arg)
 
-        elif opt in ("--contribution-file"):
+        elif opt in ("--contribution-filename"):
             contribution_file = arg
+            contribution_filenames.append(arg)
 
-        elif opt in ("--output-file"):
-            output_file = arg
+        elif opt in ("--output-filename"):
+            output_filename = arg
 
-    if individual_file == '' or family_file == '' or contribution_file == '' or output_file == '':
+    if len(individual_filenames) == 0 or len(family_filenames) == 0 or len(contribution_filenames) == 0 \
+       or output_filename == '':
         usage()
         sys.exit(2)
 
-    print 'Individual file is "' + individual_file + '"'
-    print 'Family file is "' + family_file + '"'
-    print 'Contribution file is "' + contribution_file + '"'
-    print 'Output file is "' + output_file + '"'
+    print 'Individual filename(s): ' + str(individual_filenames)
+    print 'Family filename(s): ' + str(family_filenames)
+    print 'Contribution filename(s): ' + str(contribution_filenames)
+    print 'Output file is "' + output_filename + '"'
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- sk2ccb.py
+++ sk2ccb.py
@@ -1,21 +1,57 @@
 #!/usr/bin/python
 
-import sys, getopt
+import sys, getopt, os.path, csv, argparse
 
 def main(argv):
-
-    import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument("--individual-filename", required=True, nargs='+', action='append', \
+        help="'Individual' filename (input CSV file)")
+    parser.add_argument("--individual-id-fieldname", help="'Individual' ID fieldname", default="Individual ID")
+    parser.add_argument("--family-filename", required=True, nargs='+', action='append', \
+        help="'Family' filename (input CSV file)")
     parser.add_argument("--contribution-filename", required=True, nargs='+', action='append', \
         help="'Contribution' filename (input CSV file)")
-    parser.add_argument("--individual-filename",  required=True, nargs='+', action='append', \
-        help="'Individual' filename (input CSV file)")
-    parser.add_argument("--family-filename",  required=True, nargs='+', action='append', \
-        help="'Family' filename (input CSV file)")
-    parser.add_argument("--output-filename",  required=True, help="'Output' filename (output XLS file)")
+    parser.add_argument("--output-filename", required=True, help="'Output' filename (output XLS file)")
     args = parser.parse_args()
+
+    individual_listlist = csvs2listlist(args.individual_filename[0], args.individual_id_fieldname)
 
     print str(args)
 
+def csvs2dictlist(filename_array, id_fieldname):
+    dictlist = []
+    for filename in filename_array:
+        if os.path.isfile(filename):
+            with open(filename) as csvfile:
+                csv_dict_reader = csv.DictReader(csvfile)
+                for row in csv_dict_reader:
+                    dictlist.append(row)
+            print str(dictlist)
+            return dictlist
+        else:
+            print "Error: cannot open " + filename
+
+def csvs2listlist(filename_array, id_fieldname):
+    listlist = []
+    header_count = -1
+    for filename in filename_array:
+        if os.path.isfile(filename):
+            with open(filename) as csvfile:
+                csv_reader = csv.reader(csvfile)
+                for row in csv_reader:
+                    if header_count == -1:
+                        header_count = len(row)
+                        if id_fieldname not in row:
+                            print "Error: " + str(row) + " header row does not contain required ID field named " + \
+                                id_fieldname
+                    elif len(row) != header_count:
+                        print "Error: header has " + str(header_count) + " fields, but row #x has " + str(len(row)) + \
+                            " fields"
+                    listlist.append(row)
+            print str(listlist)
+            return listlist
+        else:
+            print "Error: cannot open " + filename
+            
 if __name__ == "__main__":
     main(sys.argv[1:])
