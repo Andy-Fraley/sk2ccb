630dd4763a1431d3b7da76096528e3f55c9697de
Andy Fraley <andy_fraley@hotmail.com>
2015-07-14 15:57:46 -0400

    Created RowSet class and was going to use for loading CSV (prior to trying PETL)

--- sk2ccb.py
+++ sk2ccb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from rowset import RowSet
 import sys, getopt, os.path, csv, argparse
 
 def main(argv):
@@ -31,27 +32,34 @@ def csvs2dictlist(filename_array, id_fieldname):
         else:
             print "Error: cannot open " + filename
 
-def csvs2listlist(filename_array, id_fieldname):
+def csvs2listlist(filename_array, id_fieldname=None):
+
+    individual_rowset = RowSet(id_fieldname, *filename_array)
+#    test1_rowset = RowSet(None, 'a', 'b')
+#    test2_rowset = RowSet()
+
     listlist = []
-    header_count = -1
     for filename in filename_array:
         if os.path.isfile(filename):
             with open(filename) as csvfile:
+                row_num = 1
+                header_count = -1
                 csv_reader = csv.reader(csvfile)
                 for row in csv_reader:
                     if header_count == -1:
                         header_count = len(row)
-                        if id_fieldname not in row:
-                            print "Error: " + str(row) + " header row does not contain required ID field named " + \
-                                id_fieldname
+                        if id_fieldname is not None and id_fieldname not in row:
+                            print "Error: " + str(row) + " header row does not contain required ID field named '" + \
+                                id_fieldname + "'"
                     elif len(row) != header_count:
-                        print "Error: header has " + str(header_count) + " fields, but row #x has " + str(len(row)) + \
-                            " fields"
+                        print "Error: header has " + str(header_count) + " fields, but row #" + str(row_num) + \
+                            " has " + str(len(row)) + " fields"
                     listlist.append(row)
+                    row_num += 1
             print str(listlist)
-            return listlist
         else:
-            print "Error: cannot open " + filename
-            
+            print "Error: cannot open file '" + filename + "'"
+    return listlist
+
 if __name__ == "__main__":
     main(sys.argv[1:])
