18b3e2ca536a0edf6bb51f50abc676d5f08ec839
Andy Fraley <andy_fraley@hotmail.com>
2015-08-08 14:52:56 -0400

    Initial version of individuals transform

--- sk_individuals2ccb.py
+++ sk_individuals2ccb.py
@@ -1,83 +1,49 @@
-#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+#!/usr/bin/env python
+
 
 import sys, getopt, os.path, csv, argparse, petl
 
+
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--individual-filename", required=True, nargs='+', action='append', \
-        help="'Individual' filename (input CSV file)")
-    parser.add_argument("--individual-id-fieldname", help="'Individual' ID fieldname", default="Individual ID")
-    parser.add_argument("--family-filename", required=True, nargs='+', action='append', \
-        help="'Family' filename (input CSV file)")
-    parser.add_argument("--contribution-filename", required=True, nargs='+', action='append', \
-        help="'Contribution' filename (input CSV file)")
-    parser.add_argument("--output-filename", required=True, help="'Output' filename (output XLS file)")
+    parser.add_argument("--individuals-filename", required=True, help="Input CSV with individuals data dumped " \
+                        "from Servant Keeper")
+    parser.add_argument("--child-approvals-filename", required=True, help="Filename of CSV file listing approval " \
+                        "dates that individuals got various clearances to work with children")
+    parser.add_argument("--output-filename", required=True, help="Output CSV filename which will be loaded with " \
+                        "individuals data in CCB import format ")
     args = parser.parse_args()
 
-    individual_table = join_tables(args.individual_filename[0], args.individual_id_fieldname)
-    print individual_table
+    if not os.path.isfile(args.individuals_filename):
+        print >> sys.stderr, "Error: cannot open file '" + args.individuals_filename + "'"
+        sys.exit(1)
 
-def join_tables(filename_list, id_fieldname):
-    curr_table = None
-    for filename in filename_list:
-        if not os.path.isfile(filename):
-            print "Error: cannot open file '" + filename + "'"
-        else:
-            next_table = petl.fromcsv(filename)
-            header_row = petl.fieldnames(next_table)
-            if id_fieldname is not None and id_fieldname not in header_row:
-                print "Error: " + str(header_row) + " header row does not contain required ID field named '" + \
-                    id_fieldname + "'"
-            else:
-                if curr_table is not None:
-                    print curr_table
-                    print next_table
-                    curr_table = petl.outerjoin(curr_table, next_table, key=id_fieldname)
-                else:
-                    curr_table = next_table
-    return curr_table
+    individuals_input_table = petl.fromcsv(args.individuals_filename)
+    individuals_output_table = petl.empty()
 
-def csvs2dictlist(filename_array, id_fieldname):
-    dictlist = []
-    for filename in filename_array:
-        if os.path.isfile(filename):
-            with open(filename) as csvfile:
-                csv_dict_reader = csv.DictReader(csvfile)
-                for row in csv_dict_reader:
-                    dictlist.append(row)
-            print str(dictlist)
-            return dictlist
-        else:
-            print "Error: cannot open " + filename
+    individuals_output_table = transform_rename_rows(individuals_input_table, individuals_output_table)
 
-def csvs2listlist(filename_array, id_fieldname=None):
+    print
+    print individuals_output_table
 
-    individual_rowset = RowSet(id_fieldname, *filename_array)
-#    test1_rowset = RowSet(None, 'a', 'b')
-#    test2_rowset = RowSet()
 
-    listlist = []
-    for filename in filename_array:
-        if os.path.isfile(filename):
-            with open(filename) as csvfile:
-                row_num = 1
-                header_count = -1
-                csv_reader = csv.reader(csvfile)
-                for row in csv_reader:
-                    if header_count == -1:
-                        header_count = len(row)
-                        if id_fieldname is not None and id_fieldname not in row:
-                            print "Error: " + str(row) + " header row does not contain required ID field named '" + \
-                                id_fieldname + "'"
-                    elif len(row) != header_count:
-                        print "Error: header has " + str(header_count) + " fields, but row #" + str(row_num) + \
-                            " has " + str(len(row)) + " fields"
-                    listlist.append(row)
-                    row_num += 1
-            print str(listlist)
-        else:
-            print "Error: cannot open file '" + filename + "'"
-    return listlist
+def transform_rename_rows(individuals_input_table, individuals_output_table):
+    column_mappings = {
+        'Address': 'home street',
+        'Address Line 2': 'home street line 2',
+        'Address': 'mailing street',
+        'Address Line 2': 'mailing street line 2',
+        'Alt Address': 'other street',  # No such thing as 'other street' in silver_sample file
+        'Alt Address Line 2': 'other street line 2',
+        'Alt City': 'other city'
+    }
+
+    for key in column_mappings:
+        individuals_output_table = petl.addcolumn(individuals_output_table, column_mappings[key], \
+                                                  petl.values(individuals_input_table, key))
+
+    return individuals_output_table
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
