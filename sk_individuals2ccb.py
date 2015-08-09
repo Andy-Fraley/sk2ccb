1528ba6c1024ce7085b40032a1c4fc72fa62c368
Andy Fraley <andy_fraley@hotmail.com>
2015-08-08 20:49:50 -0400

    Got initial column rename working

--- sk_individuals2ccb.py
+++ sk_individuals2ccb.py
@@ -19,29 +19,71 @@ def main(argv):
         sys.exit(1)
 
     individuals_input_table = petl.fromcsv(args.individuals_filename)
-    individuals_output_table = petl.empty()
+    individuals_output_table = transform_rename_columns(individuals_input_table)
 
-    individuals_output_table = transform_rename_rows(individuals_input_table, individuals_output_table)
-
-    print
-    print individuals_output_table
+    petl.tocsv(individuals_output_table, args.output_filename)
 
 
-def transform_rename_rows(individuals_input_table, individuals_output_table):
-    column_mappings = {
+def transform_rename_columns(individuals_input_table):
+    column_renames = {
         'Address': 'home street',
-        'Address Line 2': 'home street line 2',
         'Address': 'mailing street',
+        'Address Line 2': 'home street line 2',
         'Address Line 2': 'mailing street line 2',
         'Alt Address': 'other street',  # No such thing as 'other street' in silver_sample file
         'Alt Address Line 2': 'other street line 2',
-        'Alt City': 'other city'
+        'Alt City': 'other city',
+        'Alt Country': 'other country',
+        'Alt State': 'other state',
+        'Alt Zip Code': 'other_postal code',
+        'Birth Date': 'birthday',
+        'Cell Phone': 'cell phone',
+        'City': 'home_city',
+        'City': 'city',
+        'Country': 'country',
+        'Date of Death': 'deceased',
+#        'Emergency Contact': 'emergency contact name',
+#        'Emergency Phone': 'emergency phone',
+        'First Name': 'legal name',
+        'Gender': 'gender',
+        'Home Phone': 'home phone',
+        'Individual e-Mail': 'email',
+        'Last Name': 'last name',
+        'Mail Box #': 'mailbox number',
+        'Marital Status': 'marital status',
+        'Middle Name': 'middle name',
+        'Occupation': 'job title',
+        'Photo Release': 'photo release',
+        'Preferred Name': 'first name',
+        'School District': 'school',
+        'State': 'state',
+        'State': 'home state',
+        'Suffix': 'suffix',
+        'Title': 'prefix',
+        'Racial/Ethnic identification': 'ethnicity',
+        'Relationship': 'family position',
+        'Wedding Date': 'anniversary',
+        'Work Phone': 'work phone',
+        'Zip Code': 'home_postal code',
+        'Zip Code': 'postal code',
+        'Env #': 'giving #',
+        'The Spirit Mailing': 'spirit mailing',
+        'Baptized': 'baptized',
+        'Baptized by': 'baptized by',
+        'Baptized Date': 'baptized date',
+        'Church Transferred From': 'church transferred from',
+        'Church Transferred To': 'church transferred to',
+        'Confirmed': 'confirmed',
+        'Confirmed Date': 'confirmed date',
+        'Date Joined': 'membership date',
+        'How Joined': 'how they joined',
+        'Member Status': 'membership type',
+        'Pastor when joined': 'pastor when joined',
+        'Pastor when leaving': 'pastor when leaving',
+        'Trf out/Withdrawal Date': 'membership stop date'
     }
 
-    for key in column_mappings:
-        individuals_output_table = petl.addcolumn(individuals_output_table, column_mappings[key], \
-                                                  petl.values(individuals_input_table, key))
-
+    individuals_output_table = petl.rename(individuals_input_table, column_renames)
     return individuals_output_table
 
 
